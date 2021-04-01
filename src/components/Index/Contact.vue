72a604c46acfd095abf76bfe393531e7cfc97c32
BryanPowell370 <47546361+LaphingMan@users.noreply.github.com>
2021-03-31 20:41:13 -0500

    Finished Contact
    Contact Finished needs more style though.

--- src/components/Index/Contact.vue
+++ src/components/Index/Contact.vue
@@ -1,27 +1,43 @@
 <template lang="">
     <div class="contactLayout">
-         <div class="titleWrapper">
+        <div class="titleWrapper">
             <div>
                 <h1 class="WaterMark">Contact</h1>
                 <h2 class="KnowMore">Get In Touch</h2>
             </div>
 
+
+            <div class="AddressLayout">
+                <ul>
+
+
+                    <li>
+                        <h1>Address</h1>
+                    </li>
+                    <li>
+                        <p>3011 Salem Church Rd, Sneads Fl</p>
+                    </li>
+                    <li>
+                        <p>(850) 372-2486</p>
+                    </li>
+
+                    <li>
+                        <p>bryan.powell370@gmail.com</p>
+                    </li>
+                    <li>
+                           <button class="button">Send Message</button> 
+                    </li>
+                </ul>
+
+                <form action="">
+                    <input placeholder="Name" type="text" name="Name" id="">
+                    <input placeholder="Email" type="email" name="Email" id="">
+                    <textarea name="" id="" cols="30" rows="10" placeholder="Tell us more about your needs........"></textarea>
              
-<div >
-    <li>
-        <h1>Address</h1>
-    </li>
-    <li>
-        <p>3011 Salem Church Rd, Sneads Fl</p>
-    </li>
-    <li>
-        <p>(850) 372-2486</p>
-    </li>
-    
-    <li>
-        <p>bryan.powell370@gmail.com</p>
-    </li>
-</div>
+                
+                </form>
+               
+            </div>
 
 
 
@@ -29,8 +45,7 @@
     </div>
 </template>
 <script>
-export default {
-    
-}
+    export default {
+
+    }
 </script>
- 
